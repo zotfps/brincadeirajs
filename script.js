056ff337b085f2a9c804f00638cdab245fcc02fd
Joao <jottamarcos77@gmail.com>
2025-08-21 21:49:46 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let resultado = document.getElementById("resultado")
+document.querySelector(".botaoMedir button").addEventListener("click", verificarBotao);
+
+function verificarBotao(){
+    let Tamanho = (Number(window.prompt("Digite o tamanho do seu pênis em cm:")));
+    resultado.innerHTML = "Medindo o pênis...";
+    setTimeout(() => {
+        if (Tamanho < 12) {
+            resultado.innerHTML = "Pênis pequeno";
+        } else if (Tamanho >= 12 && Tamanho < 18) {
+            resultado.innerHTML = "Pênis médio";
+        } else {
+            resultado.innerHTML = "Pênis grande";
+    }
+}, 1000);
+}
