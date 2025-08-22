80c6e286d6471f63c562e6169b499bf4fc6247eb
Joao <jottamarcos77@gmail.com>
2025-08-21 22:04:31 -0300

    Corrigido Bug Palavra

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ let resultado = document.getElementById("resultado")
 document.querySelector(".botaoMedir button").addEventListener("click", verificarBotao);
 
 function verificarBotao(){
+    do {
     let Tamanho = (Number(window.prompt("Digite o tamanho do seu pênis em cm:")));
+    } while (isNaN(Tamanho) || Tamanho === 0);
     resultado.innerHTML = "Medindo o pênis...";
     setTimeout(() => {
+        
         if (Tamanho < 12) {
             resultado.innerHTML = "Pênis pequeno";
         } else if (Tamanho >= 12 && Tamanho < 16) {
