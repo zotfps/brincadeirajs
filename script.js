b0a998eaf72629aeeebd8593f9a87bcdc7cd8eab
Joao <jottamarcos77@gmail.com>
2025-08-21 22:08:18 -0300

    Bug Palavra:

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-let resultado = document.getElementById("resultado")
+let resultado = document.getElementById("resultado");
 document.querySelector(".botaoMedir button").addEventListener("click", verificarBotao);
 
-function verificarBotao(){
+function verificarBotao() {
+    let Tamanho;
     do {
-    let Tamanho = (Number(window.prompt("Digite o tamanho do seu pênis em cm:")));
-    } while (isNaN(Tamanho) || Tamanho === 0);
+        Tamanho = Number(window.prompt("Digite o tamanho do seu pênis em cm:"));
+    } while (isNaN(Tamanho) || Tamanho <= 0);
+
     resultado.innerHTML = "Medindo o pênis...";
     setTimeout(() => {
-        
         if (Tamanho < 12) {
             resultado.innerHTML = "Pênis pequeno";
         } else if (Tamanho >= 12 && Tamanho < 16) {
             resultado.innerHTML = "Pênis médio";
         } else {
             resultado.innerHTML = "Pênis grande";
-    }
-}, 1000);
+        }
+    }, 1000);
 }
